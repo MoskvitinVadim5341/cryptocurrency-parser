95062f181fed8e04d710c60550581e6b5e8e3134
MoskvitinVadim5341 <95953619+MoskvitinVadim5341@users.noreply.github.com>
2022-06-24 19:01:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from unicodedata import name
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import requests
 
+
+ua = UserAgent()
+
+user_agent = ua.random
+print(f"User agent: {user_agent}")
+
+
 heder = {
-    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0",
+    "User-Agent":user_agent,
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
 }
 responose = requests.get("https://minfin.com.ua/currency/crypto/", headers=heder)
@@ -21,6 +28,3 @@ for i in range(len(criptos)):
         z += 1
     else:
         continue
-""" names = names[0:22]
-for i in names:
-    print(i.text) """
